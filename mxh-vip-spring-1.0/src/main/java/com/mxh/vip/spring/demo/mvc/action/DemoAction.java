cec81751e207c2e4c71e8d7846ece72dbf988a94
maxiaohong123 <554349712@qq.com>
2020-04-03 10:47:49 +0800

    2020项目练习专用仓库

---
+++ mxh-vip-spring-1.0/src/main/java/com/mxh/vip/spring/demo/mvc/action/DemoAction.java
@@ -0,0 +1,61 @@
+package com.mxh.vip.spring.demo.mvc.action;
+
+import com.mxh.vip.spring.demo.service.IDemoService;
+import com.mxh.vip.spring.framework.annotation.GPAutowired;
+import com.mxh.vip.spring.framework.annotation.GPController;
+import com.mxh.vip.spring.framework.annotation.GPRequestMapping;
+import com.mxh.vip.spring.framework.annotation.GPRequestParam;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+//虽然，用法一样，但是没有功能
+@GPController
+@GPRequestMapping("/demo")
+public class DemoAction {
+
+  	@GPAutowired
+	private IDemoService demoService;
+
+	@GPRequestMapping("/query")
+	public void query(HttpServletRequest req, HttpServletResponse resp,
+					  @GPRequestParam("name") String name){
+		String result = demoService.get(name);
+//		String result = "My name is " + name;
+		try {
+			resp.getWriter().write(result);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@GPRequestMapping("/add")
+	public void add(HttpServletRequest req, HttpServletResponse resp,
+					@GPRequestParam("a") Integer a, @GPRequestParam("b") Integer b){
+		try {
+			resp.getWriter().write(a + "+" + b + "=" + (a + b));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@GPRequestMapping("/sub")
+	public void sub(HttpServletRequest req, HttpServletResponse resp,
+					@GPRequestParam("a") Double a, @GPRequestParam("b") Double b){
+		try {
+			resp.getWriter().write(a + "-" + b + "=" + (a - b));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@GPRequestMapping("/remove")
+	public String  remove(@GPRequestParam("id") Integer id){
+		return "" + id;
+	}
+
+}
